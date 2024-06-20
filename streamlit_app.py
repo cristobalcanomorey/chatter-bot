4bba5c1794cf396d9744412b8bd329a9ef3445e1
cristobalcanomorey <56203466+cristobalcanomorey@users.noreply.github.com>
2024-06-20 16:55:27 +0000

    Working Extractive QA solution with RoBERTa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,147 +1,173 @@
 import streamlit as st
-# import replicate
-import os
-import subprocess
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
-import time
-import requests
+# Load pre-trained model for understanding user queries
+@st.cache_resource
+def load_model(model_name):
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return model, tokenizer, nlp
 
-# Function to run a command in the shell
-def run_command(command):
-    """Run a command in the shell."""
-    process = subprocess.Popen(command, shell=True)
-    return process
+model_name = "deepset/roberta-base-squad2"
+model, tokenizer, nlp = load_model(model_name)
+manual_context = None
+curriculum_data = {
+    "Especialization": "Cristóbal Cano is specialized in an AI and Big Data with a background in web development.",
+    "Education": "He completed a specialized course in AI and Big Data at IEDIB (2023-2024), a higher degree in web application development at Ies Son Ferrer (2018-2020), and a medium degree in microcomputer systems and networks at Ies Son Ferrer (2015-2018).",
+    "AI_skills": "His AI skills include machine learning, deep learning, model deployment, data preprocessing, natural language processing, computer vision, and reinforcement learning.",
+    "AI_tools": "He is proficient with tools like Gradio, Streamlit, Python, Docker, Poetry, Flask, Azure, and Knime.",
+    "Big_Data_skills": "In Big Data, he has skills in data visualization, distributed computing, data quality monitoring, data warehousing, and business intelligence.",
+    "Big_Data_Tools": " He works with tools such as PowerBI, ETL, OLTP/OLAP, Neo4j, MongoDB, Apache Hadoop, Azure Databricks, and others.",
+    "Web_developer_skills": "As a web developer, he has experience with HTML, CSS, JavaScript, PHP, MySQL, and various frameworks like WordPress and Joomla.",
+    "Job_experience": "He has professional experience as a web application developer at IB3 Televisió (July 2022 - July 2023), where he implemented new web designs and developed applications using PHP and WordPress.",
+    "Languages_spoken": "He is fluent in Catalan and Spanish, has intermediate English proficiency",
+    "Hobbies": "He enjoys playing the electric guitar, watching YouTube, and dancing Salsa and Bachata.",
+    "Contact_information": "Contact: tofolcanodam@gmail.com, LinkedIn: https://www.linkedin.com/in/cristobal-cano-morey/"
+}
 
-# Function to check if the server is running
-def check_server(url):
-    """Check if the server at the given URL is up."""
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return True
-    except requests.ConnectionError:
-        return False
-    return False
+# Summarized Curriculum
+# summarized_curriculum = """
+# Cristóbal Cano is an AI and Big Data specialist with a background in web development. 
+# He completed a specialized course in AI and Big Data at IEDIB (2023-2024), a higher degree in web application development at Ies Son Ferrer (2018-2020), 
+# and a medium degree in microcomputer systems and networks at Ies Son Ferrer (2015-2018).
 
-# Ensure setup commands run only once
-if 'setup_done' not in st.session_state:
-    st.session_state.setup_done = False
+# His AI skills include machine learning, deep learning, model deployment, data preprocessing, natural language processing, computer vision, and reinforcement learning. 
+# He is proficient with tools like Gradio, Streamlit, Python, Docker, Poetry, Flask, Azure, and Knime.
 
-if not st.session_state.setup_done:
-    # Streamlit app
-    st.title("Ollama Chatbot Setup and Interface")
-    # Run the installation script and wait for it to complete
-    with st.spinner("Installing Ollama..."):
-        st.markdown("### Running installation script...")
-        subprocess.run("curl -fsSL https://ollama.com/install.sh | sh", shell=True, check=True)
-        st.markdown("### Installation script completed.")
+# In Big Data, he has skills in data visualization, distributed computing, data quality monitoring, data warehousing, and business intelligence. 
+# He works with tools such as PowerBI, ETL, OLTP/OLAP, Neo4j, MongoDB, Apache Hadoop, Azure Databricks, and others.
 
-        # Start the ollama server in the background
-        st.markdown("### Starting ollama server...")
-        ollama_server = run_command("ollama serve")
+# As a web developer, he has experience with HTML, CSS, JavaScript, PHP, MySQL, and various frameworks like WordPress and Joomla. 
+# He has professional experience as a web application developer at IB3 Televisió (July 2022 - July 2023), where he implemented new web designs and developed applications using PHP and WordPress.
 
-        # Wait until the ollama server is up and running
-        while not check_server("http://127.0.0.1:11434"):
-            st.markdown("Waiting for ollama server to start...")
-            time.sleep(5)
+# He is fluent in Catalan and Spanish, has intermediate English proficiency, and enjoys playing the electric guitar, watching YouTube, and dancing Salsa and Bachata.
 
-        st.markdown("### Ollama server is up and running.")
-
-        # Pull llama3 in the background
-        st.markdown("### Pulling llama3 model...")
-        pull_llama3 = run_command("ollama pull llama3")
-
-        # Wait for the pull command to complete
-        pull_llama3.wait()
-
-        st.markdown("### llama3 model has been pulled successfully.")
-
-        # Mark setup as done
-        st.session_state.setup_done = True
-
-# import ollama
-from langchain_community.llms import Ollama
-
-llm = Ollama(model='llama3')
-
-# st.set_page_config(
-#     layout="wide"
-# )
+# Contact: tofolcanodam@gmail.com, LinkedIn: https://www.linkedin.com/in/cristobal-cano-morey/
+# """
 
 if st.button("Clear Messages"):
     st.session_state.messages.clear()
 
-# Function to run a shell command and print its output
-def run_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    for line in process.stdout:
-        print(line, end='')
-    for line in process.stderr:
-        print(line, end='')
-    process.wait()
-    stdout, stderr = process.communicate()
-    if process.returncode != 0:
-        st.error(f"Error occurred: {stderr.strip()}{stdout}")
-    else:
-        st.success("Command executed successfully.")
-
-prompt = ""
-
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {
-        "role": "assistant",
-        "content": "Hello! I am a helpful assistant."
+            "role": "assistant",
+            "content": "Hello! I am a helpful assistant. You can ask me questions about Cristóbal Cano's curriculum."
         }
     ]
-    # Command to install Ollama
-    # install_command = "curl -fsSL https://ollama.com/install.sh | sh"
-    # with st.spinner("Installing Ollama..."):
-    #     run_command(install_command)
 
-    # # Command to serve Ollama
-    # serve_command = "ollama serve"
-    # with st.spinner("Starting Ollama server..."):
-    #     subprocess.Popen(serve_command, shell=True)  # Running this in the background
-
-    # # Command to pull the Llama3 model
-    # pull_command = "ollama pull llama3"
-    # with st.spinner("Downloading llama3..."):
-    #     run_command(pull_command)
-    
-    
-    
-
-# streamlit run streamlit_app.py --server.enableCORS false --server.enableXsrfProtection false
-
-
+# Display chat messages from session state
 for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
-        prompt += f'{message["role"]} said: {message["content"]}\n '
 
-sent = st.chat_input('Ask me anything')
+sent = st.chat_input('Ask me anything about the curriculum')
+
+def list_to_natural_language(lst):
+    if not lst:
+        return "empty list"
+    elif len(lst) == 1:
+        return lst[0]
+    else:
+        return ', '.join(lst[:-1]) + ' and ' + lst[-1]
+
+list_of_contexts = list(curriculum_data.keys())
+available_categories = list_to_natural_language(list_of_contexts)
+
+def get_context(prompt):
+    query = f'What is the correct category for the following question? Question: "{prompt}"'
+    model_name = "deepset/roberta-base-squad2"
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    QA_input = {
+        'question': query,
+        'context': available_categories
+    }
+    res = nlp(QA_input)
+
+    context_obtained = res['answer']
+
+    print(f'Context: {context_obtained}')
+    context = context_obtained
+    return curriculum_data[context] if context in list_of_contexts else None
+
+def generate_response(prompt, context):
+    model_name = "deepset/roberta-base-squad2"
+    if context:
+        nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+        QA_input = {
+            'question': prompt,
+            'context': context
+        }
+        res = nlp(QA_input)
+        full_res = f'AI Generated response: {res["answer"]}'
+        return full_res, context
+    else:
+        return None, None
+
+def send_AI_response(response, clarification):
+    with st.chat_message(name='assistant'):
+        st.write(response)
+        st.write(clarification)
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": response
+    })
 
 if sent:
-    prompt += f'user said: {sent}\n'
     with st.chat_message('user'):
         st.markdown(sent)
 
-    user_prompt = {
+    user_message = {
         "role": "user",
         "content": sent
     }
-    st.session_state.messages.append(user_prompt)
+    st.session_state.messages.append(user_message)
 
-    prompt += 'assistant said:'
     with st.spinner("Generating response..."):
-        chat_response = llm.invoke(prompt, stop=['<|eot_id|>'])
-        response = f'{chat_response}'
-        with st.chat_message(name='assistant'):
-            st.write(response)
-        
-        st.session_state.messages.append({
-            "role": "assistant",
-            "content": response
-        })
+                
+        #TODONE:
+        # - If contexto manual:
+            # Generar respuesta 
+        # - else:
+            # - Obtener contexto por IA
+            # - If contexto bueno:
+                # Generar respuesta
+            # - else:
+                # Formulario de contexto
 
+        if manual_context:
+            context = manual_context
+            manual_context = None
+            response, context = generate_response(sent,context)
+            clarification = f'Context: "{context}"'
+            send_AI_response(response, clarification)
+        else:
+            context = get_context(sent)
+            if context:
+                response, context = generate_response(sent,context)
+                clarification = f'Context: "{context}"'
+                send_AI_response(response, clarification)
+            else:
+                send_AI_response(
+                    "Sorry to inform that I couldn't understand what you're talking about",
+                    "Please select one of the options:"
+                )
+                list_expanded = [''] + list_of_contexts
+                with st.form(key="Manual context"):
+                    manual_context = st.selectbox(
+                        "I'm not the smartest AI... What is this question about?",
+                        tuple(list_expanded),
+                        key = 'context_selectbox')
+                    submit_button = st.form_submit_button(label='Submit')
+                    if submit_button:
+                        print(f'Manual context: {manual_context}')
+                        context = manual_context
+                        response, context = generate_response(sent,context)
+                        clarification = f'Context: "{context}"'
+                        send_AI_response(response, clarification)
+          
+
+            
+                        
+        
